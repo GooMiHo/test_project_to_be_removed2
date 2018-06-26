f263b291a218f1577699b939c660fcc079bf65a9
Bethany Walker <reklaw.bethany@gmail.com>
2018-06-26 19:45:42 -0400

    test2

--- recipe-converter-gh-pages/src/App.js
+++ recipe-converter-gh-pages/src/App.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ingrArray} from './ingredient-conv-chart';
+import {ingrArray} from './ingredient-conv-chart';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 
 function convertCupRemainders(remainingCups, fullCups){
@@ -318,5 +318,4 @@ document.getElementById("pressed-button").addEventListener("click", function() {
         alert("ingredient is not avaliable");
     }
 }, false);
-
 export default App;
